c7794b5e5643d8f4ba31e963847693d8af94f6e0
BoltonAndrew <boltonandrew@live.co.uk>
2023-10-09 21:03:04 +0100

    cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("../db/connection.js");
 const itemRouter = require("./routes/item.js");
 const categoryRouter = require("./routes/category.js");
 
-const port = 5001;
+const port = process.env.PORT || 5001;
 const app = express();
 
+app.use(cors()); // Allows access to the API from other apps.
 app.use(express.json());
 app.use("/item", itemRouter);
 app.use("/category", categoryRouter);
